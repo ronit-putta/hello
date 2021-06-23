6d254dea4a94744aedef4a600a27cd5001fbef92
ronit-putta <86378193+ronit-putta@users.noreply.github.com>
2021-06-24 00:54:56 +0530

    Add files via upload

---
+++ MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+     <div>
+       <h1>Ronit</h1>
+       <p>I wanna be the very best, that no one ever was. To catch them is my quest, to teach them is my goal.</p>
+       <ol>
+         <li>America</li>
+         <li>Maldives</li>
+         <li>Japan</li>
+       </ol>
+     </div>
+   )
+}
+
+export default MyInfo
